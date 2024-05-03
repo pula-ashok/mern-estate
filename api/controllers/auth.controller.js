6352f854e8f415fa5bb8ade7552d293eead4144a
pula-ashok <ashokpula0307@gmail.com>
2024-05-03 10:33:33 +0530

    complete signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,17 @@ import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
-    const userfound = await User.findOne({ email });
+    const userfound = await User.findOne({ email: email });
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return next(errorHandler(400, "All fields are required"));
+    }
     if (userfound) {
       return next(errorHandler(400, "User already registered"));
     }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const signupSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch(`/api/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+      } else {
+        setLoading(false);
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form action="" className="flex flex-col gap-4" onSubmit={signupSubmit}>
+        <input
+          type="text"
+          className="border p-3 rounded-lg"
+          placeholder="Username"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          className="border p-3 rounded-lg"
+          placeholder="Email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          className="border p-3 rounded-lg"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          type="submit"
+        >
+          {loading ? "Loading..." : " Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-700 mt-5">{error}</p>}
+    </div>
+  );
 };
 
 export default SignUp;
