2535fb78fa8dd13c50c889fd76c684f9c3ffc841
pula-ashok <ashokpula0307@gmail.com>
2024-05-03 11:03:47 +0530

    sign in completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import bcryptjs from "bcryptjs";
 import User from "./../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
-    const userfound = await User.findOne({ email: email });
     if (
       !username ||
       !email ||
@@ -16,6 +16,8 @@ export const signup = async (req, res, next) => {
     ) {
       return next(errorHandler(400, "All fields are required"));
     }
+    const userfound = await User.findOne({ email: email });
+
     if (userfound) {
       return next(errorHandler(400, "User already registered"));
     }
@@ -23,6 +25,33 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: "Signup successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password || email === "" || password === "") {
+      return next(errorHandler(400, "All fields are required"));
+    }
+    const userfound = await User.findOne({ email });
+    if (!userfound) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const passwordMatch = bcryptjs.compareSync(password, userfound.password);
+    if (!passwordMatch) {
+      return next(errorHandler(404, "Password incorrect"));
+    }
+    const token = jwt.sign({ id: userfound._id }, process.env.JWT_SECRET);
+    const { password: userpassword, ...rest } = userfound._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
